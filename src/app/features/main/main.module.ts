34f8b6f3ec3466761cc9b8b8b34ab7ee6600c4b9
Victor Hugo <vpereira648@gmail.com>
2021-09-19 22:53:20 -0300

    IBEIECM-1909 - componentizando menu e criando breakpoints

--- src/app/features/main/main.module.ts
+++ src/app/features/main/main.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { MatIconModule } from '@angular/material';
+import { HeaderModule } from 'src/app/shared/components/header/header.module';
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    HeaderModule
   ],
   exports: [MainComponent]
 })
